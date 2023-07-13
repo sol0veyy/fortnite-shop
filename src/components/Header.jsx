5a697f9496a3c87f9dad923730a4abc609140e5e
sol0vey <solovevdaniil03@mail.ru>
2023-07-13 15:50:37 +0300

    change name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
       <nav className="green darken-1">
           <div className="nav-wrapper">
-              <a href="/" className="brand-logo">React Shop</a>
+              <a href="/" className="brand-logo">Fortnite Shop</a>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                   <li><a href="https://github.com/sol0veyy/fortnite-shop" target="_blank" rel="noreferrer">Repo</a></li>
               </ul>
